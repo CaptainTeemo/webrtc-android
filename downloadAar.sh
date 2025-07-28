dd6f69a7ea028ef052a3b356f19418b697a72705
Captain Teemo <alittlebarney@gmail.com>
2025-07-28 21:28:48 +0800

    Update downloadAar.sh

--- downloadAar.sh
+++ downloadAar.sh
@@ -6,7 +6,7 @@ set -x
 # Get VERSION_NAME from gradle.properties
 VERSION=`grep -o 'VERSION_NAME=.*' gradle.properties | cut -f2- -d=`
 
-SDK_BIN_URL=https://github.com/webrtc-sdk/android/releases/download/v${VERSION}/libwebrtc.aar
+SDK_BIN_URL=https://github.com/CaptainTeemo/webrtc-android/releases/download/v${VERSION}/libwebrtc.aar
 
 echo "Downloading webrtc-sdk ${VERSION} binary for android."
 curl -f -L -o "android/libwebrtc.aar" ${SDK_BIN_URL}
